e2faed3616536472bb33ba4f1048c9de0f084273
Ashley Camba Garrido <ashwoods@gmail.com>
2020-06-03 11:16:03 +0200

    Update test boilerplate

--- setup.py
+++ setup.py
@@ -5,54 +5,46 @@ mixtape: awesome mix vol 1 -> python asyncio gstreamer application mini-framewor
 
 """
 from setuptools import setup, find_packages
-from os.path import dirname, abspath
-import os.path
 
-PARENT_DIR = dirname(abspath(__file__))
-
-def get_version():  # move versioning into __init__.py
-    return "0.4.1"
 
 test_deps = [
-    'pytest',
-    'colorlog',
-    'pytest-cov',
-    'pytest-flake8',
-    'flake8-bugbear',
-    'pytest-black',
-    'pytest-asyncio',
-    'pytest-benchmark',
-    'pytest-profiling',
-    'pytest-leaks',
-    'pytest-bandit',
-    'memory_profiler',
-    'pytest-xdist',
-    'teamcity-messages',
-    'pdbpp',
+    "pytest==5.4.2",
+    "pytest-asyncio==0.12.0",
+    "pytest-benchmark==3.2.3",
+    "pytest-black==0.3.9",
+    "pytest-cov==2.9.0",
+    "pytest-flake8==1.0.6",
+    "pytest-forked==1.1.3",
+    "pytest-leaks==0.3.1",
+    "pytest-mock==3.1.0",
+    "pytest-mypy==0.6.2",
+    "pytest-mypy-plugins==1.3.0",
+    "pytest-profiling==1.7.0",
+    "colorlog==4.1.0",
+    "flake8-bugbear==20.1.4",
+    "PyGObject-stubs==0.0.2",
+    "pdbpp",
 ]
 extras = {
-    'test': test_deps,
+    "test": test_deps,
 }
 
 setup(
     name="mixtape",
-    version=get_version(),
+    version="0.5.0.dev0",
     author="Ashley Camba Garrido",
     author_email="ashwoods@gmail.com",
     url="https://github.com/ashwoods/mixtape",
     description="Gstreamer python application mini-framework",
     long_description=__doc__,
     packages=find_packages(exclude=("tests", "tests.*")),
+    # PEP 561
+    package_data={"mixtape": ["py.typed"]},
     zip_safe=False,
     license="MIT",
     tests_require=test_deps,
     extras_require=extras,
-    install_requires=[
-        'attrs',
-        'pkgconfig',
-        'pampy',
-        'beppu'
-        ],
+    install_requires=["attrs", "pampy", "beppu"],
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
         "Intended Audience :: Developers",
@@ -61,6 +53,7 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
 )
